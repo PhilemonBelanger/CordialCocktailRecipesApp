caada9f01b939eae14214770eaf787cde520b74a
Philemon Belanger <philemonbelanger@gmail.com>
2024-08-28 12:51:31 -0400

    chore: changed version number to 1.0.1

--- app.config.js
+++ app.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "Cordial",
   slug: "cordial-cocktail-recipes-app",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
